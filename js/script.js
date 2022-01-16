d76c22f8321094be60055aed9cde193e6495186d
alhamidysattam <Alhamidysattam@gmail.com>
2022-01-16 10:03:36 +0300

    added html , css and JS (F-header section)
    1- add mouse tracking
    2- change the nav images based on the nav item
    3- show nav bar and image when click the burger menu
    
    finished header section :)

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+
+let cursor = document.querySelector('.cursor');
+
+
+// mouse tracking
+window.onmousemove = (e) =>{
+       cursor.style.top = e.pageY + 'px';
+       cursor.style.left = e.pageX + 'px';
+}
+
+//change the nav images based on the nav item
+document.querySelectorAll('.navbar a ').forEach( link => {
+       link.onmouseenter = () =>{
+              document.querySelector('.navbar-img img').src = link.getAttribute('data-src');
+       };
+       link.onmouseleave = () =>{
+              document.querySelector('.navbar-img img').src = "imges/home.png";
+       };
+});
+
+//show nav bar and image when click the burger menu
+let navbar = document.querySelector('.navbar');
+let navbarImg = document.querySelector('.navbar-img');
+
+document.querySelector('#menu-btn').onclick = () => {
+       navbar.classList.toggle('active');
+       navbarImg.classList.toggle('active');
+};
+
+
+//remove nav bar and image (and use media queries in CSS to show them agin)
+/* for testing 
+window.onscroll = () => {
+       navbar.classList.remove('active');
+       navbarImg.classList.remove('active');
+};
+
+*/
